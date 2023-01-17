145d0d5e5cb63cbd6b75e2de056dca88fba6a95e
Aqil-Shahzad <122356773+Aqil-Shahzad@users.noreply.github.com>
2023-01-17 19:21:00 +0500

    Working Button

--- form.js
+++ form.js
@@ -1,43 +1,75 @@
-
-function myFunction(){
-      const element=prompt("Write for, title.");
-      document.getElementById("demo").innerHTML=element;
+function myFunction() {
+  const lbl = document.createElement("h2");
+  const element = prompt("Write a Title:");
+  lbl.innerText = element;
+  document.body.appendChild(lbl);
 }
-
-
 
 function myFunction1() {
-const lbl=document.createElement("label");
-const element=prompt("Write Lable:");
-lbl.innerText=element;
-const lbl1=document.createElement("input");
-const lbl2=document.createElement("br");
-// document.getElementById("s2").innerHTML=lbl;
-// document.getElementById("s2").innerHTML=lbl1;
-document.body.appendChild(lbl);
-document.body.appendChild(lbl1);
-document.body.appendChild(lbl2);
+  const lbl = document.createElement("label");
+  const element = prompt("Write Lable:");
+  lbl.innerText = element;
+  const lbl1 = document.createElement("input");
+  const lbl2 = document.createElement("br");
+  const lbl3 = document.createElement("br");
+  document.body.appendChild(lbl);
+  document.body.appendChild(lbl3);
+  document.body.appendChild(lbl1);
+  document.body.appendChild(lbl2);
 }
-// function myFunction1()
-// {
-//     var tag = document.createElement(p);
-//       var text = document.createTextNode("Tutorix is the best e-learning platform");
-//       tag.appendChild(text);
-//       var element = document.getElementById("firstF");
-//       element.appendChild(tag);
-// }
-// function changeText3(){
-//  var oldHTML = document.getElementById('para').innerHTML;
-//  var newHTML = "<span style='color:#ffffff'>" + oldHTML + "</span>";
-//  document.getElementById('para').innerHTML = newHTML;
-// }
-/* <script>
-      var tag = document.createElement("input");
-      var text = document.createTextNode("Tutorix is the best e-learning platform");
-      tag.appendChild(text);
-      var element = document.getElementById("new");
-      element.appendChild(tag);
-   </script> */
+function myFunction2() {
+  const lbl = document.createElement("label");
+  const element = prompt("Write Lable:");
+  lbl.innerText = element;
+  const lbl1 = document.createElement("textarea");
+  const lbl2 = document.createElement("br");
+  const lbl3 = document.createElement("br");
+  document.body.appendChild(lbl);
+  document.body.appendChild(lbl2);
+  document.body.appendChild(lbl1);
+  document.body.appendChild(lbl3);
+}
+
+function myFunction3() {
+      
+      const lbl = document.createElement("label");
+      const element = prompt("Write Lable:");
+      lbl.innerText = element;
+      const lbl2 = document.createElement("br");
+      
+      const option1 = document.createElement("label");
+      const op1 = prompt("Write Lable:");
+      option1.innerText = op1;
+
+      var lbl1 = document.createElement('input');
+      lbl1.type = "radio";
+      const lbl3 = document.createElement("br");
+      document.body.appendChild(lbl);
+      document.body.appendChild(lbl2);
+      document.body.appendChild(lbl1);
+      document.body.appendChild(option1);
+      document.body.appendChild(lbl3);
+    }
+
+function myFunction4() {
+      
+  const lbl = document.createElement("label");
+  const element = prompt("Write Lable:");
+  lbl.innerText = element;
+  
+  var lbl1 = document.createElement('input');
+  lbl1.type = "checkbox";
+  const lbl2 = document.createElement("br");
+  document.body.appendChild(lbl);
+  document.body.appendChild(lbl1);
+  document.body.appendChild(lbl2);
+}
 
 
-   
+
+
+checkbox.type = "checkbox";
+            checkbox.name = "name";
+            checkbox.value = "value";
+            checkbox.id = "id";
+            
